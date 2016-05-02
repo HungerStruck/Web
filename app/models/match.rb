f2e4beb1816f83d1977b8c67db827df92fda1743
Kailan Blanks <kailan@kmp.pw>
2016-05-02 22:28:23 +0100

    Add a couple more values to the Match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,6 +2,9 @@ class Match
   include Mongoid::Document
 
   field :time, type: Date
+  field :state, type: Symbol # either :playing or :ended
+
   field :map,  type: String # will be map object eventually
+  field :server, type: String # e.g hs7
 
 end
