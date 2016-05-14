3560b24740ac9c5aefb96639c02abc7bb94087e6
LeonKoole <leon@koole.io>
2016-05-14 14:46:35 +0200

    one line solution for last commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
 module ApplicationHelper
   def active_controller(controller)
-    if(params[:controller] == controller)
-      return 'active'
-    end
+    return 'active' if params[:controller] == controller
   end
 end
