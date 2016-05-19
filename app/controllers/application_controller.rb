24aa8854f8d382c1c03853721722c213cba4e094
Trevor <hi@trvr.xyz>
2016-05-19 15:43:45 -0400

    Add initializer, change views to show footer rev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :get_revision
+
+  private
+  def get_revision
+    if defined? $g
+      @revision = $g.log[0]
+    end
+  end
+  
 end
