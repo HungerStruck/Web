7c6646f86cc5a7a89eef8de8b9c22f354a373800
Trevor <hi@trvr.xyz>
2016-05-24 10:14:41 -0400

    Better page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def active_controller(controller)
     return 'active' if params[:controller] == controller
   end
+  def title(page_title)
+    content_for :title, "HungerStruck » #{page_title.to_s}"
+  end
 end
