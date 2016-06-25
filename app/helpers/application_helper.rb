2b4acad5c889a1981660315d6f5640c7bab5252c
LeonKoole <leon@koole.io>
2016-06-25 22:10:23 +0200

    if the user is not found, dont show tooltip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,8 @@ module ApplicationHelper
   end
 
   def user_tooltip_element(username, link, strong)
-    # Needs to get these values from database
-    kills = 50
-    deaths = 40
-    rank = 30
-    background = 1
+    user = User.where(username: username)
 
-    snippet = ""
     start = ""
     if strong == true && link == false
       start = '%strong{ '
@@ -21,8 +16,19 @@ module ApplicationHelper
     else
       start = '%a{ href: "/p/' + username + '", '
     end
-    
-    haml = start + '"data-toggle" => "popover", "title" => "' + username + '", "data-placement" => "top", "data-content" => "player_tooltip", "data-kills" => "' + kills.to_s + '", "data-deaths" => "' + deaths.to_s + '", "data-rank" => "' + rank.to_s + '", "data-bg" => "' + background.to_s + '"} ' + username
+
+    if user.length > 0
+      kills = 50
+      deaths = 40
+      rank = 30
+      background = 1
+
+      snippet = ""
+      
+      haml = start + '"data-toggle" => "popover", "title" => "' + username + '", "data-placement" => "top", "data-content" => "player_tooltip", "data-kills" => "' + kills.to_s + '", "data-deaths" => "' + deaths.to_s + '", "data-rank" => "' + rank.to_s + '", "data-bg" => "' + background.to_s + '"} ' + username
+    else
+      haml = start + '} ' + username
+    end
     snippet = Haml::Engine.new(haml)
     return snippet.render
   end
@@ -32,7 +38,7 @@ module ApplicationHelper
     snippet = Haml::Engine.new(haml)
     return snippet.render
   end
-  
+
   def title(page_title)
     content_for :title, "HungerStruck » #{page_title.to_s}"
   end
