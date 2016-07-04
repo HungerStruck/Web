cc203c295f01886e5975d64b6d6921ebb838b2d1
skipperguy12 <neiljohari@gmail.com>
2016-07-04 12:31:36 -0400

    Use Redis client and store everything into a hash instead of one large string

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,30 +3,29 @@ class StatsController < ApplicationController
   def index
     @stats_users = case  params[:period]
       when "today"    #compare to 1
-        Rails.cache.fetch("stats_users_today", expires_in: 1.day) do
-         User.only([:username, :kills, :deaths, :wins, :losses]).to_a.each do |user|
-          zero_stats(user)
-         end
-        end
+        get_stats_or_insert("today")
       when "week"    #compare to 2
-        Rails.cache.fetch("stats_users_week", expires_in: 1.week) do
-          User.only([:username, :kills, :deaths, :wins, :losses]).to_a.each do |user|
-           zero_stats(user)
-          end
-        end
+        get_stats_or_insert("week")
       else
-        Rails.cache.fetch("stats_users", expires_in: 30.minutes) do
-         User.only([:username, :kills, :deaths, :wins, :losses]).to_a
-        end
+        get_stats_or_insert("all")
       end
   end
 
   private
-  def zero_stats(user)
-    user.kills = 0
-    user.deaths = 0
-    user.wins = 0
-    user.losses = 0
+  def get_stats_or_insert(period)
+    ret_val = Array.new
+
+    $redis.scan_each(match: "cache:stats_users_#{period}:*") do |resume_key_name|
+        ret_val.push($redis.hgetall(resume_key_name))
+    end
+
+    if ret_val.empty? # Likely will not happen on eral server because the DB plugin will insert records and thus ret_val will NOT be empty. In the case it is, every user will be dumped into redis.
+      User.all.each do |user|
+        $redis.hmset("cache:stats_users_#{period}:#{user.uuid}", "username", user.username, "kills", user.kills, "deaths", user.deaths, "wins", user.wins, "losses", user.losses)
+      end
+    end
+
+    ret_val
   end
 
 end
