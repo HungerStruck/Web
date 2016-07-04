959f07d2048b9097dcd5498c2c4df3955ffaa7ad
skipperguy12 <neiljohari@gmail.com>
2016-07-04 14:19:14 -0400

    Add only descending sorting on stat columns

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,22 +1,28 @@
 class StatsController < ApplicationController
 
   def index
-    @stats_users = case  params[:period]
+    puts stats_params
+    @stats_users = case  stats_params[:period]
       when "today"    #compare to 1
-        get_stats_or_insert("today")
+        get_stats_or_insert("today", stats_params[:sort])
       when "week"    #compare to 2
-        get_stats_or_insert("week")
+        get_stats_or_insert("week", stats_params[:sort])
       else
-        get_stats_or_insert("all")
+        get_stats_or_insert("all", stats_params[:sort])
       end
   end
 
   private
-  def get_stats_or_insert(period)
+  def stats_params
+    params.permit(:sort, :period)
+  end
+
+
+  def get_stats_or_insert(period, sort)
     ret_val = Array.new
 
     $redis.scan_each(match: "cache:stats_users_#{period}:*") do |resume_key_name|
-        ret_val.push($redis.hgetall(resume_key_name))
+        ret_val.push($redis.hgetall(resume_key_name).to_h)
     end
 
     if ret_val.empty? # Likely will not happen on eral server because the DB plugin will insert records and thus ret_val will NOT be empty. In the case it is, every user will be dumped into redis.
@@ -24,8 +30,7 @@ class StatsController < ApplicationController
         $redis.hmset("cache:stats_users_#{period}:#{user.uuid}", "username", user.username, "kills", user.kills, "deaths", user.deaths, "wins", user.wins, "losses", user.losses)
       end
     end
-
-    ret_val
+    ret_val.sort_by! { |hash| hash[sort].to_i }
   end
 
 end
