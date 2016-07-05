78a4da049f695c7812e19df46ac3298cbd3dd341
skipperguy12 <neiljohari@gmail.com>
2016-07-04 20:56:28 -0400

    Store 2 less keys per user, slightly more optimized for ram use

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -31,21 +31,34 @@ class StatsController < ApplicationController
     params.permit(:sort, :period, :direction, :page)
   end
 
+  def initialize_user_redis_stats(user)
+    $redis.hmset("cache:stats_users:#{user.uuid}", "username", user.username, "kills", user.kills, "deaths", user.deaths, "wins", user.wins, "losses", user.losses)
+    $redis.hmset("cache:stats_users:#{user.uuid}", "username", user.username, "kills_week", user.kills, "deaths_week", user.deaths, "wins_week", user.wins, "losses_week", user.losses)
+    $redis.hmset("cache:stats_users:#{user.uuid}", "username", user.username, "kills_today", user.kills, "deaths_today", user.deaths, "wins_today", user.wins, "losses_today", user.losses)
+  end
 
   def get_stats_or_insert(period, sort)
     ret_val = Array.new
 
-    $redis.scan_each(match: "cache:stats_users_#{period}:*") do |resume_key_name|
+    $redis.scan_each(match: "cache:stats_users:*") do |resume_key_name|
         user_hash = $redis.hgetall(resume_key_name).to_h
+        if user_hash["kills_#{period}"]
+          user_hash["kills"] = user_hash.delete "kills_#{period}"
+          user_hash["deaths"] = user_hash.delete "deaths_#{period}"
+          user_hash["wins"] = user_hash.delete "wins_#{period}"
+          user_hash["losses"] = user_hash.delete "losses_#{period}"
+        end
+
         # Non redis cached, but avoid doing math in the view
         user_hash["kd"] = (user_hash["kills"].to_f / user_hash["deaths"].to_i).round(2)
         user_hash["wl"] = (user_hash["wins"].to_f / user_hash["losses"].to_i).round(2)
+
         ret_val.push(user_hash)
     end
 
     if ret_val.empty? # Likely will not happen on prod server because the DB plugin will insert records and thus ret_val will NOT be empty. In the case it is, every user will be dumped into redis.
       User.all.each do |user|
-        $redis.hmset("cache:stats_users_#{period}:#{user.uuid}", "username", user.username, "kills", user.kills, "deaths", user.deaths, "wins", user.wins, "losses", user.losses)
+        initialize_user_redis_stats(user)
       end
     end
 
