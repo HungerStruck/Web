d7286871cfef73b36f7078d1807b4eec27ee775f
LeonKoole <leon@koole.io>
2016-07-17 20:01:52 +0200

    Don't scroll if the quoted message is already in view

--- app/assets/javascripts/reply_to.js
+++ app/assets/javascripts/reply_to.js
@@ -1,5 +1,4 @@
 document.addEventListener("turbolinks:load", function() {
-  var fadedPosts = false;
   $('[data-reply]').on( "click", function() {
     var quoted = $(this).attr('data-reply');
     if ( $( "#" + quoted ).length ) {
@@ -10,15 +9,25 @@ document.addEventListener("turbolinks:load", function() {
       scrollduration = Math.sqrt(scrollduration) * 20;
       $( ".post:not(#" + quoted +")").css('opacity', '0.3')
       $( "#" + quoted).css('transform', 'scale(1.05)');
-      $('html, body').animate({
-          scrollTop: ($( "#" + quoted ).offset().top - 50)
-      }, {
-        duration: scrollduration,
-        complete: function() {
-          fadedPosts = true;
-          setTimeout(function(){ $( ".post").css('opacity', '1').css('transform', 'scale(1)'); }, 500);
-        }
-      });
+
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+
+      var elemTop = $(this).parent().offset().top;
+      var elemBottom = elemTop + $(this).parent().height();
+
+      if((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
+        setTimeout(function(){ $( ".post").css('opacity', '1').css('transform', 'scale(1)'); }, 500);
+      } else {
+        $('html, body').animate({
+            scrollTop: ($( "#" + quoted ).offset().top - 50)
+        }, {
+          duration: scrollduration,
+          complete: function() {
+            setTimeout(function(){ $( ".post").css('opacity', '1').css('transform', 'scale(1)'); }, 500);
+          }
+        });
+      }
     } else {
       $( this ).parent().find(".reply-quote").show();
     }
